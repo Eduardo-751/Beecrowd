bd276e278885cb09ae9804d44219139840b74966
Eduardo-751 <edu.s.camargo97@gmail.com>
2023-06-13 10:27:42 -0300

    AD-HOC C# Solutions

--- AD-HOC/1087 - Queen/1087.cs
+++ AD-HOC/1087 - Queen/1087.cs
@@ -1,35 +1,28 @@
-using System;
+using System; 
 
-class URI
-{
-    static void Main(string[] args)
-    {
-        int N = 1;
-        while (N != 0)
+class URI {
+
+    static void Main(string[] args) {
+        while (true)
         {
-            N = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int x1 = int.Parse(input[0]);
+            int y1 = int.Parse(input[1]);
+            int x2 = int.Parse(input[2]);
+            int y2 = int.Parse(input[3]);
 
-            if (N == 0)
+            if (x1 == 0 && y1 == 0 && x2 == 0 && y2 == 0)
                 break;
 
-            int[] samples = new int[N + 2];
-            string[] input = Console.ReadLine().Split(' ');
-            for (int i = 0; i < N; i++)
-            {
-                samples[i] = int.Parse(input[i]);
-            }
-            samples[N] = int.Parse(input[0]);
-            samples[N + 1] = int.Parse(input[1]);
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
 
-            int waves = 0;
-
-            for (int i = 1; i < N + 2 - 1; i++)
-            {
-                if ((samples[i] > samples[i - 1] && samples[i] > samples[i + 1]) || (samples[i] < samples[i - 1] && samples[i] < samples[i + 1]))
-                    waves++;
-            }
-
-            Console.WriteLine(waves);
+            if (dx == 0 && dy == 0)
+                Console.WriteLine(0);
+            else if (dx == dy || dx == 0 || dy == 0)
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(2);
         }
     }
 }
--- AD-HOC/1089 - Musical Loop/README.md
+++ AD-HOC/1089 - Musical Loop/README.md
@@ -64,3 +64,36 @@ int main()
     return 0;
 }
 ```
+### C#
+
+```cs
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int N = 1;
+        while (N != 0)
+        {
+            N = int.Parse(Console.ReadLine());
+    
+            if (N == 0)
+                break;
+
+            int[] samples = new int[N];
+
+            samples = int.Parse(Console.ReadLine().Split(' '));
+
+            int waves = 0;
+
+            for (int i = 1; i < N - 1; i++)
+            {
+                if ((samples[i] > samples[i - 1] && samples[i] > samples[i + 1]) || (samples[i] < samples[i - 1] && samples[i] < samples[i + 1]))
+                    waves++;
+            }
+
+            Console.WriteLine(waves);
+        }
+    }
+}
+```
